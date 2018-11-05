de0de9e5ac21035b1d1e45cd308f629e6200052a
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-05 12:25:44 -0500

    added-instructions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//  INSTRUCTIONS QUESTION
+//  An anagram is a word, phrase, or sentence formed from another by rearranging its letters. An example of this is "angel", which is an anagram of "glean".
+
+// Given an array of words write a method that will return the total number of different anagrams inside that array. You can only count once the anagram between 2 words. For instance, in the previous example we show "angel" and "glean" as anagrams, but you don't have to count the opposite between "glean" and "angel".
+
+// Some examples:
+
+// There are 2 anagrams in the array ["dell", "ledl", "abc", "cba"]
+// There are 7 anagrams in the array ["dell", "ledl", "abc", "cba", "bca", "bac"]
+
 let words = ['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']
 // great explanation of function with answer!
 
@@ -33,3 +43,5 @@ return numberOfAnagrams
 }
 
 console.log(anagramCounter(words));
+
+// Best Practices: 
